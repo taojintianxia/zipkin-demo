a8b69b879a07e6627f3a150bca09140b41e63f1f
nianjun.sun <sunnianjun@rongcapital.cn>
2017-04-13 16:18:19 +0800

    修复调用关系

--- zipkin-sample1/src/main/java/com/github/taojintianxia/controller/HomeController.java
+++ zipkin-sample1/src/main/java/com/github/taojintianxia/controller/HomeController.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.github.taojintianxia.config.BasicConfig;
 import com.github.taojintianxia.config.ZipkinProperties;
@@ -31,12 +32,14 @@ public class HomeController {
 
     private Random random = new Random();
 
-    private static final int MAX_WORKING_TIME = 1000;
+    private static final int MAX_WORKING_TIME = 500;
 
     @RequestMapping("start")
+    @ResponseBody
     public String start() throws InterruptedException, IOException {
-        String result = zipkinProperties.getServiceName() + " is executing";
+        String result = zipkinProperties.getServiceName() + " is executing \n";
         int sleep = random.nextInt(MAX_WORKING_TIME);
+        result += zipkinProperties.getServiceName() + " works on " + sleep + " ms";
         TimeUnit.MILLISECONDS.sleep(sleep);
         List<String> uris = basicConfig.getRedirectURI();
         if (uris.isEmpty()) {
@@ -45,11 +48,10 @@ public class HomeController {
             for (String uri : uris) {
                 Request request = new Request.Builder().url(uri).get().build();
                 Response response = client.newCall(request).execute();
-                result = result + "\n " + zipkinProperties.getServiceName() + " works on " + sleep + " ms]"
-                        + response.body().toString();
+                result = result + response.body().string();
             }
         }
-        System.out.println("----------------:" + uris.get(1));
-        return null;
+
+        return result;
     }
 }
